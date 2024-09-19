3a9a6526224fdfccab053ff5316e2e7654bc4628
AidaIsakovic <aidisa664@ihmeducation.se>
2024-09-19 16:09:40 +0200

    update a1 project

--- A1-project/js/app.js
+++ A1-project/js/app.js
@@ -6,6 +6,8 @@ let button0 = document.getElementById("button0");
 let playerRollText = document.getElementById("playerRollText");
 let aiRollText = document.getElementById("aiRollText");
 let resultText = document.getElementById("resultText");
+let playerScoreText = document.getElementById("playerScore");
+let aiScoreText = document.getElementById("aiScore");
 
 // DATA
 let playerRoll= getRandomNumberOneToSixPlayer();
@@ -14,31 +16,36 @@ let aiRoll = getRandomNumberOneToSixAi();
 console.log(aiRoll);
 
 //SCORE
-
+let playerScore = 0;
+let aiScore = 0;
 
 //PROCESS
 button0.addEventListener("click", function(){
-
   getRandomNumberOneToSixPlayer();
-  showPlayerRollResult();
+  showPlayerResult();
   getRandomNumberOneToSixAi();
   showAiResult();
 
 });
 
 //CONTROLLERS
-function getRandomNumberOneToSixPlayer(){
+function getRandomNumberOneToSixPlayer() {
   return Math.floor(Math.random() * 6) + 1;
 }
-function getRandomNumberOneToSixAi(){
+function getRandomNumberOneToSixAi() {
   return Math.floor(Math.random() * 6) + 1;
 }
 
 
 //views
 function showPlayerRollResult(){
-  playerRollText.innerHTML = playerRoll;
+  playerScoreText.innerHTML = playerRoll;
 }
+
 function showAiResult(){
-  aiRollText.innerHTML = aiRoll;
+  aiScoreText.innerHTML = aiRoll;
+}
+
+function showResultText(){
+  resultText.innerHTML = resultText;
 }
